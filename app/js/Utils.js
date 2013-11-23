f815899d40d4eea042fa3190a1224e223afd6fdc
Ernesta <ernesta@ernes7a.lt>
2013-11-24 00:24:10 +0100

    Added methods for computing weighted mean and skewness.

--- app/js/Utils.js
+++ app/js/Utils.js
@@ -9,6 +9,16 @@ var Utils = (function($) {
 		return sum / values.length;
 	}
 	
+	function computeWeightedMean(values) {
+		var sum = 0;
+		
+		for (var i = 0; i < values.length; i++) {
+			sum += (values.length - i) * values[i];
+		}
+		
+		return sum / values.length;
+	}
+	
 	function computeDeviation(values) {
 		var mean = computeMean(values);
 		var sum = 0;
@@ -20,9 +30,32 @@ var Utils = (function($) {
 		return Math.sqrt(sum / values.length);
 	}
 	
+	function computeSkewness(values) {
+		var m2 = 0;
+		var m3 = 0;
+		
+		var mean = computeMean(values);
+		var n = values.length;
+		
+		for (var i = 0; i < n; i++) {
+			m2 += Math.pow(values[i] - mean, 2);
+			m3 += Math.pow(values[i] - mean, 3);
+		}
+		
+		m2 = m2 / n;
+		m3 = m3 / n;
+		
+		var skew = Math.pow(m3 / m2, 1.5);
+		skew = (Math.sqrt(n * (n - 1)) * skew) / (n - 2);
+		
+		return skew;
+	}
+	
 	
 	return {
 		computeMean: computeMean,
-		computeDeviation: computeDeviation
+		computeWeightedMean: computeWeightedMean,
+		computeDeviation: computeDeviation,
+		computeSkewness: computeSkewness
 	};
 })(jQuery);
