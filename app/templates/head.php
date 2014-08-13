16b8af14fa2a70a5bf0e90c5dd174f68e67e0bf0
Ernesta Orlovaitė <ernesta@ernes7a.lt>
2014-08-13 19:59:52 +0300

    Added Google Analytics.

--- app/templates/head.php
+++ app/templates/head.php
@@ -10,9 +10,12 @@
 
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/app.css">
-
+	
 	<!--[if lt IE 9]>
 		<!--<script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>-->
 		<!--<script src="//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>-->
 	<![endif]-->
+	
+	<?php include_once("templates/analytics.php"); ?>
+	
 </head>
